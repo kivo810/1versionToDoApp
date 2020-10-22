7b7612c10a78d8fcd1a2512eba85e845e212f29a
kivo810 <kivo810@hotmail.com>
2020-10-22 12:10:00 +0200

    Fixed color Tag and s**ts I found

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :user, required: true
-  has_many :tag_assocations, dependent: :destroy
-  has_many :tasks, through: :tag_assocations
+  has_many :tag_associations, dependent: :destroy
+  has_many :tasks, through: :tag_associations
 
   validates :title, presence: true
   validates :color, presence: true
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,8 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :category, optional: true
 
-  has_many :tag_assocations, dependent: :destroy
-  has_many :tags, :through => :tag_assocations
+  has_many :tag_associations, dependent: :destroy
+  has_many :tags, :through => :tag_associations
 
   validates :title, presence: true
 end
